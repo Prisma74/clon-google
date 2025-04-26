6d3b2e117bb3e84cb020cacbd0363b4789095c46
Prisma74 <solopipe2001@gmail.com>
2025-04-25 21:24:19 -0500

    Add component navBar y styles NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import styles from "./NavBar.module.css"
+
+export const NavBar = () => {
+  const items = ["Correo", "Imagenes", "Icon", "Photo"];
+
+  return (
+    <header>
+      <nav>
+        <ul className={styles.navRightSection}>
+          {items.map((item, index) => (
+            <li key={index}>
+              <a href="">{item}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
