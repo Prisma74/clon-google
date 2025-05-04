3b33f97fdf9e22df83a54ef27f337aa99ca1b5ac
Prisma74 <solopipe2001@gmail.com>
2025-05-04 18:26:40 -0500

    styles last item (photo-perfil)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import styles from "./NavBar.module.css"
 
 export const NavBar = () => {
@@ -9,7 +10,10 @@ export const NavBar = () => {
         <ul className={styles.navRightSection}>
           {items.map((item, index) => (
             <li key={index}>
-              <a href="" className={index === 2 ? styles.iconOnly : "" }>{item}</a>
+              <a href="" className={clsx({
+                [styles.iconOnly] : index === 2,
+                [styles.photoOnly] : index === 3
+              })}>{item}</a>
             </li>
           ))}
         </ul>
