227a16f1a6e54650d407a59412c8157c0b375afb
Prisma74 <solopipe2001@gmail.com>
2025-05-09 20:24:20 -0500

    Update xd.js

---
+++ src/xd.js
@@ -0,0 +1,16 @@
+let nombreUsuario ="Camilo";
+let edadUsuario = parseInt("25");
+let esEstudiante = ("si").toLowerCase() == "sí";
+let mensaje = `Hola, ${nombreUsuario}`; 
+mensaje +=  `Tienes ${edadUsuario} años`;
+
+if (edadUsuario >= 18) {
+ mensaje += " Eres mayor de edad.";
+} else {
+ mensaje += " Eres menor de edad.";
+}
+console.log(esEstudiante);
+if (esEstudiante) {
+ mensaje += " Tienes derecho a un descuento por ser estudiant.";
+}
+console.log(mensaje);
